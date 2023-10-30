86a3898dfc32c66db69d99229db4ff718a42cfb4
rajiv <rajeev11153@gmail.com>
2023-10-30 13:35:59 +0545

    installation

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Route::get('/{pathMatch}', function () {
     return view('welcome');
 })->where('pathMatch', ".*");
+
+
+
+// Npm install
+// npm install vue-loader@next vue@next vue-router@next --force
+// npm install @vitejs/plugin-vue --force --save-dev
+// Npm run dev
